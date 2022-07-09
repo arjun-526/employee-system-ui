0ae44c89f8bb616649d9ad206c80c7cdfb9ad7fb
Arjun526 <nagarjuna.anke2@gmail.com>
2022-07-09 17:56:55 +0530

    code optimization and save back and reset button

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import EmployeeService from "../services/EmployeeService";
+import Employee from "./Employee";
 
 const EmployeeList = () => {
   const navigate = useNavigate();
@@ -55,28 +56,7 @@ const EmployeeList = () => {
           {!loading && (
           <tbody className="bg-white">
             {employees.map((employee) => (
-            <tr key={employee.id}>
-              <td className="text-left px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-500">{employee.firstName}</div>
-              </td>
-              <td className="text-left px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-500">{employee.lastName}</div>
-              </td>
-              <td className="text-left px-6 py-4 whitespace-nowrap">
-                <div className="text-sm text-gray-500">{employee.emailId}</div>
-              </td>
-              <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-                <a
-                  href="#"
-                  className="text-indigo-600 hover:text-indigo-800 px-4"
-                >
-                  Edit
-                </a>
-                <a href="#" className="text-indigo-600 hover:text-indigo-800">
-                  Delete
-                </a>
-              </td>
-            </tr>
+            <Employee employee={employee} key={employee.id}></Employee>
             ))}
           </tbody>
           )}
